1d09e1c4cb31a0e88f6dbfb6e92c647a7a8cb80b
Gabba <gmedas@8thlight.com>
2018-02-15 10:50:42 +0000

    tests homepage display

--- app.rb
+++ app.rb
@@ -3,13 +3,35 @@ require_relative 'lib/hangman_rules'
 require_relative 'lib/word_generator'
 require_relative 'lib/words'
 require_relative 'lib/word_formatter'
-require_relative 'controller_helpers'
 
-class Controller < Sinatra::Base
+class App < Sinatra::Base
 
   enable :sessions
 
-  helpers ControllerHelpers
+  helpers do
+
+    def letters
+      if session[:letters].nil?
+	session[:letters] = rules.guessable_letters
+      end
+      session[:letters]
+    end
+
+    def secret_word
+      if session[:secret_word].nil?
+	session[:secret_word] = rules.secret_word
+      end
+      session[:secret_word]
+    end
+
+    def rules
+      HangmanRules.new(WordGenerator.new(Words.new))
+    end
+
+    def word_formatter
+      WordFormatter.new(rules)
+    end
+  end
 
   get "/" do
     @secret_word = word_formatter.format(secret_word)
